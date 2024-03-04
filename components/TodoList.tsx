9de07dbfb5c97e76414b9e3841a162c051928465
shuuuu10-01 <koron.konan123@gmail.com>
2024-03-04 19:15:21 +0900

    feat: ソート順を入れ替えるライブラリの導入

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,32 +1,53 @@
 import { Todo } from "@/types/todo";
-import { Text, View } from "./Themed";
-import { StyleSheet } from "react-native";
-import React from "react";
+import { Text } from "./Themed";
+import { Pressable, StyleSheet, TouchableOpacity, View } from "react-native";
+
+import DraggableFlatList, {
+  ScaleDecorator,
+  RenderItemParams,
+} from "react-native-draggable-flatlist";
+import { FontAwesome } from "@expo/vector-icons";
 
 export function TodoList({ todo }: { todo: Todo[] }) {
+  const Item = ({ item, drag, isActive }: RenderItemParams<Todo>) => {
+    return (
+      <ScaleDecorator>
+        <TouchableOpacity activeOpacity={1} disabled={isActive} style={styles.item}>
+          <Text>{item.title}</Text>
+          <Pressable onTouchMove={drag}>
+            {({ pressed }) => (
+              <FontAwesome
+                name="plus"
+                size={30}
+                color={"black"}
+                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+              />
+            )}
+          </Pressable>
+        </TouchableOpacity>
+      </ScaleDecorator>
+    );
+  };
   return (
     <View style={styles.container}>
-      {todo.map((t) => {
-        return <Item key={t.id} item={t} />;
-      })}
-    </View>
-  );
-}
-
-function Item({ item }: { item: Todo }) {
-  return (
-    <View lightColor="#fff" darkColor="#ccc" style={styles.item}>
-      <Text>{item.title}</Text>
+      <DraggableFlatList
+        data={todo}
+        onDragEnd={({ data }) => console.log(data)}
+        keyExtractor={(item) => item.id}
+        renderItem={Item}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "80%",
+    width: "90%",
+    paddingVertical: 30,
   },
   item: {
     width: "100%",
+    flex: 1,
     height: 50,
     display: "flex",
     alignItems: "flex-start",
