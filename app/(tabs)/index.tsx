e2dafc569b435105d42c8a5252e08e5145f6d578
shuuuu10-01 <koron.konan123@gmail.com>
2024-03-10 21:58:07 +0900

    delete: 初期ページの不要な要素の削除

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,18 @@
 import { Pressable, StyleSheet } from "react-native";
 
-import { View, TextInput } from "@/components/Themed";
-import React, { useState } from "react";
+import { View } from "@/components/Themed";
+import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
-import { actions, selectors, useAppDispatch, useAppSelector } from "@/redux";
 import { TodoList } from "@/components/TodoList";
 import { Link } from "expo-router";
 
 export default function TabOneScreen() {
-  const todo = useAppSelector((state) => selectors.todo.sampleSelector(state.todo));
-  const dispatch = useAppDispatch();
-  const [title, setTitle] = useState("");
   const colorScheme = useColorScheme();
-
-  const handlePress = () => {
-    if (title === "") return;
-    dispatch(
-      actions.todo.add({
-        title: title,
-        categoryId: "1",
-        completed: false,
-      }),
-    );
-    setTitle("");
-  };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={setTitle}
-        blurOnSubmit
-        enterKeyHint="done"
-        placeholder="TODOを入力してください"
-      />
-      <Pressable onPress={handlePress}>
-        {({ pressed }) => (
-          <FontAwesome
-            name="plus"
-            size={30}
-            color={Colors[colorScheme ?? "light"].text}
-            style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-          />
-        )}
-      </Pressable>
       <TodoList />
       <Link href="/modal" asChild>
         <Pressable style={styles.add}>
@@ -70,14 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
-  },
-  input: {
-    width: "80%",
-    height: 40,
-    padding: 10,
-    margin: 12,
-    borderWidth: 0.5,
-    borderRadius: 4,
+    paddingTop: 15,
   },
   add: {
     position: "absolute",
