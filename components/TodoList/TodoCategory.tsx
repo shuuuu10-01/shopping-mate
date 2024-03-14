f72654c8a9b5849ea68af995088ab0a06554e927
shuuuu10-01 <koron.konan123@gmail.com>
2024-03-14 22:21:04 +0900

    feat: カテゴリー別にコンポーネントを切り分け

---
+++ components/TodoList/TodoCategory.tsx
@@ -0,0 +1,46 @@
+import { Category } from "@/types/categry";
+import { Text } from "../Themed";
+import { StyleSheet } from "react-native";
+import { NestableDraggableFlatList } from "react-native-draggable-flatlist";
+import { Todo } from "@/types/todo";
+import { actions, selectors, useAppDispatch, useAppSelector } from "@/redux";
+import { Item } from "./Item";
+
+export function TodoCategory({ category }: { category: Category }) {
+  const dispatch = useAppDispatch();
+  const handleDragEnd = (dragEndTodo: Todo[]) => {
+    const converted: Todo[] = dragEndTodo.map((d, index) => {
+      return {
+        ...d,
+        order: index,
+      };
+    });
+    dispatch(actions.todo.setMany(converted));
+  };
+
+  const items = useAppSelector((state) => selectors.todo.sampleSelector(state.todo));
+  return (
+    <>
+      <Text style={styles.categoryName}>{category.name}</Text>
+      <NestableDraggableFlatList<Todo>
+        data={items}
+        onDragEnd={({ data }) => handleDragEnd(data)}
+        keyExtractor={({ id }) => id}
+        renderItem={Item}
+        style={styles.container}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  categoryName: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  container: {
+    width: "100%",
+    padding: 0,
+    marginBottom: 20,
+  },
+});
--- components/TodoList/index.tsx
+++ components/TodoList/index.tsx
@@ -1,48 +1,14 @@
-import { Todo } from "@/types/todo";
-import { Text } from "@/components/Themed";
 import { StyleSheet, View } from "react-native";
-import {
-  NestableDraggableFlatList,
-  NestableScrollContainer,
-} from "react-native-draggable-flatlist";
-import { actions, selectors, useAppDispatch, useAppSelector } from "@/redux";
-import { useMemo } from "react";
+import { NestableScrollContainer } from "react-native-draggable-flatlist";
 import { CATEGORIES } from "@/constants/category";
-import { Item } from "./Item";
+import { TodoCategory } from "./TodoCategory";
 
 export function TodoList() {
-  const todo = useAppSelector((state) => selectors.todo.sampleSelector(state.todo));
-  const ordered = useMemo(() => {
-    return [...todo].sort((a, b) => (a.order > b.order ? 1 : -1));
-  }, [todo]);
-
-  const dispatch = useAppDispatch();
-  const handleDragEnd = (dragEndTodo: Todo[]) => {
-    const converted: Todo[] = dragEndTodo.map((d, index) => {
-      return {
-        ...d,
-        order: index,
-      };
-    });
-    dispatch(actions.todo.setMany(converted));
-  };
-
   return (
     <View style={styles.wrapper}>
       <NestableScrollContainer>
         {CATEGORIES.map((c) => {
-          return (
-            <>
-              <Text style={styles.categoryName}>{c.name}</Text>
-              <NestableDraggableFlatList<Todo>
-                data={ordered}
-                onDragEnd={({ data }) => handleDragEnd(data)}
-                keyExtractor={({ id }) => id}
-                renderItem={Item}
-                style={styles.container}
-              />
-            </>
-          );
+          return <TodoCategory key={c.id} category={c} />;
         })}
       </NestableScrollContainer>
     </View>
@@ -54,14 +20,5 @@ const styles = StyleSheet.create({
     width: "80%",
     display: "flex",
     alignItems: "center",
-  },
-  categoryName: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  container: {
-    width: "100%",
-    padding: 0,
-    marginBottom: 20,
   },
 });
