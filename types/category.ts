54ef33d54549fb170276da08cda0cd4b5affdcf0
shuuuu10-01 <koron.konan123@gmail.com>
2024-03-19 22:56:45 +0900

    feat: Categoryの型にcolorを追加

--- types/category.ts
+++ types/category.ts
@@ -1,5 +1,10 @@
+import { EntityState } from "@reduxjs/toolkit";
+
 export type Category = {
   id: string;
   name: string;
   order: number;
+  color: string;
 };
+
+export type SerializedCategory = EntityState<Category, string>;
